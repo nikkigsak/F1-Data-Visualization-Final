713be17b1145c2715c388878f456e8d5752d61e1
christa <christa.chiao@gmail.com>
2022-05-09 09:26:09 -0400

    final style guide colors

---
+++ driver_colors.R
@@ -0,0 +1,48 @@
+2019:
+  c("#1E41FF", # Alex Albon (Red Bull)
+    "#9B0000", # Antonio Giovinazzi (Alfa Romeo)
+    "#FF8700", # Carlos Sainz (2019 at McL; 2021 at RB)
+    "#DC0000", # LeCler; Ferrari
+    "#FFF500", # Ricciardo Renault 2019, 2021 at McL
+    "#469BFF", # Kvyat at Toro Roso/Alpha Tauri
+    "#FFFFFF", # George Russell (williams 2019, mb 2021)
+    "#F0D787", # K Mag (Haas)
+    "#9B0000", # Kimi at Alfa Romeo
+    "#F596C8", # Stroll; Racing Point 2019; AM 2021
+    "#FF8700", # Norris, McLaren
+    "#00D2BE", # Lewis Hamilton; Mercedes
+    "#1E41FF", # Max Verstappen; Red Bull
+    "#FFF500", # Nico Hulkenberg; Renault 2019
+    "#469BFF", # Gasly Toro Rosso 2019/Alpha Tauri 2021
+    "#FFFFFF", # Kubica Williams 2019, none 2021
+    "#F0D787", # Grosjean Haas 2019; none 2021
+    "#DC0000", # Vettel Ferrari 2019; Aston Martin 2021
+    "#F596C8", # Perez, Racing Point 2019, RB 2021
+    "#00D2BE")) # Bottas Merc
+
+
+2021:
+  c("#B12039", # Antonio Giovinazzi (Alfa Romeo)
+    "#ED1C24", # Carlos Sainz (2019 at McL; 2021 at RB)
+    "#ED1C24", # LeClerc; Ferrari
+    "#F58020", # Ricciardo Renault 2019, 2021 at McL
+    "#2293D1", # Ocon, Alpine 2021
+    "#2293D1", # Alonso, Alpine 2021
+    "#6CD3BF", # George Russell mercedes 2021
+    "#B12039", # Kimi at Alfa Romeo
+    "#2D826D", # Stroll; Racing Point 2019; AM 2021
+    "#F58020", # Norris, McLaren
+    "#6CD3BF", # Lewis Hamilton; Mercedes
+    "#1E5BC6", # Max Verstappen; Red Bull
+    "#B6BABD", # Mick Schumacher; Haas
+    "#37BEDD", # Latifi, Williams 2021
+    "#B6BABD", # Mazepin Haas
+    "#4E7C9B", # Gasly Toro Rosso 2019/Alpha Tauri 2021
+    "#37BEDD", # Kubica Williams 2019, none 2021
+    "#2D826D", # Vettel Ferrari 2019; Aston Martin 2021
+    "#1E5BC6", # Perez, Racing Point 2019, RB 2021
+    "#6CD3BF", # Bottas Merc
+    "#4E7C9B" # Yuki!! Tsonoda! Alpha Tauri
+  ))
+
+
